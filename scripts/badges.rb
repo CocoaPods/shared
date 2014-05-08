421aa9b2325ec56d5f014c0657c856e61c1a4fe6
Fabio Pelosin <fabiopelosin@gmail.com>
2014-05-08 11:40:56 +0200

    Fix badges script

--- scripts/badges.rb
+++ scripts/badges.rb
@@ -5,9 +5,9 @@ unless repo_slug
 end
 
 output = <<-DOC
-[![Build Status](https://img.shields.io/travis/CocoaPods/#{repo_slug}/master.svg?style=flat)](https://travis-ci.org/CocoaPods/CLAide)
-[![Code Climate](https://img.shields.io/codeclimate/github/CocoaPods/#{repo_slug}.svg?style=flat)](https://codeclimate.com/github/CocoaPods/CLAide)
-[![Coverage](https://img.shields.io/codeclimate/coverage/github/CocoaPods/#{repo_slug}.svg?style=flat)](https://codeclimate.com/github/CocoaPods/CLAide)
+[![Build Status](https://img.shields.io/travis/CocoaPods/#{repo_slug}/master.svg?style=flat)](https://travis-ci.org/CocoaPods/#{repo_slug})
+[![Coverage](https://img.shields.io/codeclimate/coverage/github/CocoaPods/#{repo_slug}.svg?style=flat)](https://codeclimate.com/github/CocoaPods/#{repo_slug})
+[![Code Climate](https://img.shields.io/codeclimate/github/CocoaPods/#{repo_slug}.svg?style=flat)](https://codeclimate.com/github/CocoaPods/#{repo_slug})
 DOC
 
 puts output
